60e9ab58f846ab9d637e9921808395129c1e4bbc
efaber314 <efaber1@umbc.edu>
2023-10-31 12:25:02 -0400

    i dont care any more i will drag and drop

---
+++ Wind_and_Dust.py
@@ -0,0 +1,21 @@
+#Wind AND dust analysis code
+#takes in FAUCART generated co-located files
+
+'''
+WIND ANALYSIS:
+1.histograms (after combine subhourly)
+2.monthly means (240)
+3.monthly means (12)
+4.one:one plots
+5.bubble plots
+
+DUST ANALYSIS
+6.20-year emission block plots
+7.dust emission bar and line
+8.emission change map
+
+STATISTICAL ANALYSIS
+9.ztest scores
+
+'''
+
